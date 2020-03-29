714c145c491e2b1eeb73da9fd4c39a99cf8dc4fe
tripor <figorifico3000@gmail.com>
2020-03-29 18:53:36 +0100

    Fixed index naming

--- indexes.sql
+++ indexes.sql
@@ -1,5 +1,12 @@
 Create Index candidates_year on zcandidates (year);
-Create index students_enroll_year on zstudents (enroll_year);
+Create index students_id_program_enroll_year on zstudents (id,program,enroll_year);
 
 Create bitmap index candidates_result on zcandidates (result);
-Create bitmap index candidates_status on zstudents (status);
+Create bitmap index students_status on zstudents (status);
+
+
+
+Drop index candidates_year ;
+Drop index students_enroll_year;
+Drop index candidates_result;
+Drop index students_status;
