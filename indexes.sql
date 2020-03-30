6182ed0a0cd5759d82b5df6f20dd11c6a407cd11
susanalima <up201603634@fe.up.pt>
2020-03-30 14:20:41 +0100

    Improve indexes

--- indexes.sql
+++ indexes.sql
@@ -1,12 +1,17 @@
-Create Index candidates_year on zcandidates (year);
-Create index students_id_program_enroll_year on zstudents (id,program,enroll_year);
+create index students_program_status_conclusion_enroll on zstudents (program, status, conclusion_year - enroll_year) /*1*/
 
-Create bitmap index candidates_result on zcandidates (result);
-Create bitmap index students_status on zstudents (status);
+create index students_program_enroll_year on zstudents (id, program,enroll_year); /*3*/
+
+create bitmap index candidates_result on zcandidates (result); /*5*/
+
+create bitmap index candidates_program_year_result on zcandidates (program, year, result); /*6*/
+
+create index students_program_enroll on zstudents (program, enroll_year) /*6*/
 
 
-
-Drop index candidates_year ;
-Drop index students_enroll_year;
+Drop index students_status_conclusion_enroll ;
+Drop index students_id_program_enroll_year;
 Drop index candidates_result;
-Drop index students_status;
+Drop index candidates_program_year_result;
+Drop index students_program_enroll;
+
