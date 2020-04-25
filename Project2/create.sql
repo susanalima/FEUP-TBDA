e87f96ebd70e5c66d6bea36755223db7c4c46e8e
susanalima <up201603634@fe.up.pt>
2020-04-25 17:20:22 +0100

    fix small typo

--- Project2/create.sql
+++ Project2/create.sql
@@ -1,3 +1,21 @@
+
+drop type tiposAula_t force;
+drop type dsd_t force;
+drop type docente_dsd_tab_t force;
+drop type docentes_t force;
+drop type tiposAula_dsd_tab_t force;
+drop type tiposAula_tab_t force;
+drop type ocorrencias_t force;
+drop type ocorrencias_tab_t force;
+drop type ucs_t force;
+
+drop table tiposAula;
+drop table docentes;
+drop table ocorrencias;
+drop table ucs;
+drop table dsd;
+
+
 create type tiposAula_t as object(
     tipo varchar(2),
     turnos number(2),
@@ -5,6 +23,7 @@ create type tiposAula_t as object(
     horas_turno number(5)
 );
 
+/*DOCENTE_DSD_t*/
 create type dsd_t as object(
     horas number(10),
     fator number(2),
@@ -31,7 +50,7 @@ alter type tiposAula_t add attribute tiposAula_dsd tiposAula_dsd_tab_t cascade;
 
 create type tiposAula_tab_t as table of tiposAula_t;
 
-create type ocurrencias_t as object(
+create type ocorrencias_t as object(
     ano_letivo number(4),
     periodo varchar(2),
     inscritos number(8),
@@ -43,17 +62,18 @@ create type ocurrencias_t as object(
     tiposAula tiposAula_tab_t
 );
 
-create type ocurrencias_tab_t as table of ocurrencias_t;
+create type ocorrencias_tab_t as table of ocorrencias_t;
 
 create type ucs_t as object(
     codigo varchar(7),
     designacao varchar(15),
     sigla_uc varchar(5),
     curso varchar(15),
-    ocurrencias ocurrencias_tab_t
+    ocorrencias ocorrencias_tab_t
 );
 
 
+create table dsd of dsd_t;
 
 create table tiposAula of tiposAula_t
     nested table tiposAula_dsd store as tiposAula_dsd_tab;
@@ -61,8 +81,8 @@ create table tiposAula of tiposAula_t
 create table docentes of docentes_t
     nested table docente_dsd store as docente_dsd_tab;
 
-create table ocurrencias of ocurrencias_t
+create table ocorrencias of ocorrencias_t
     nested table tiposAula store as tiposAula_tab return as locator;
 
 create table ucs of ucs_t
-    nested table ocurrencias store as ocurrencias_tab return as locator;
+    nested table ocorrencias store as ocorrencias_tab return as locator;
