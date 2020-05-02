cee6e4da8ae6a53c475d7a15d805463c7965ebde
susanalima <up201603634@fe.up.pt>
2020-05-02 15:34:57 +0100

    add possible solution for c

--- Project2/queries.sql
+++ Project2/queries.sql
@@ -12,22 +12,44 @@ group by value(ta).tipo
 
 /*c*/
 
-select value(o).ano_letivo, value(d).categoria
-from ocorrencias o, table(value(o).tiposAula) ta, table(value(ta).tiposAula_dsd) d
+with aux as (
+select value(d).nr as nr, value(d).nome as nome, value(ta).tipo as tipo, sum(value(x).horas) as horas
+from ocorrencias o, table(value(o).tiposAula) ta, table(value(ta).tiposAula_dsd) d, table(value(d).docente_dsd) x
 where value(o).ano_letivo = '2003/2004'
+and value(ta).id = value(x).id
+group by value(d).nr, value(d).nome, value(ta).tipo
+order by value(d).nr
+)
 
+select  ht.tipo, max(ht.horas)
+from aux ht
+group by ht.tipo
+ /********************/
 
-select value(d).nr, value(d).nome,  value(o).ano_letivo, value(d).categoria
-from ocorrencias o, table(value(o).tiposAula) ta, table(value(ta).tiposAula_dsd) d
+with horasPorTipo as (
+select value(d).nr as nr, value(d).nome as nome, value(ta).tipo as tipo, sum(value(x).horas) as horas
+from ocorrencias o, table(value(o).tiposAula) ta, table(value(ta).tiposAula_dsd) d, table(value(d).docente_dsd) x
 where value(o).ano_letivo = '2003/2004'
+and value(ta).id = value(x).id
+group by value(d).nr, value(d).nome, value(ta).tipo
+order by value(d).nr
+), maxHorasPorTipo as (
+select  ht.tipo as tipo, max(ht.horas) as maxHoras
+from horasPorTipo ht
+group by ht.tipo
+)
 
+select ht.nr, ht.nome, ht.tipo, ht.horas
+from horasPorTipo ht, maxHorasPorTipo mh
+where ht.tipo  = mh.tipo
+and ht.horas = mh.maxHoras;
 
 
 /*d*/
-
-select value(o).ano_letivo, value(d).categoria, round(avg(value(x).horas),3)
+select value(o).ano_letivo, value(d).categoria,  round(avg(value(x).horas),3)
 from ocorrencias o, table(value(o).tiposAula) ta, table(value(ta).tiposAula_dsd) d, table(value(d).docente_dsd) x
 where regexp_like (value(o).ano_letivo, '^200[1-4]')
+and value(ta).id = value(x).id
 group by value(o).ano_letivo, value(d).categoria 
 order by value(o).ano_letivo,  value(d).categoria 
 
