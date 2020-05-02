0394b0b1cdf2325f362677813f760aebd807c9e0
susanalima <up201603634@fe.up.pt>
2020-05-02 17:29:00 +0100

    small fix

--- Project2/queries.sql
+++ Project2/queries.sql
@@ -1,7 +1,7 @@
 
 /*a*/
 
-select value(ta).tipo,  sum(value(ta).n_aulas * value(ta).turnos)
+select value(ta).tipo,  sum(value(ta).horas_turno * value(ta).turnos)
 from ucs u, table(value(u).ocorrencias) o, table(value(o).tiposAula) ta
 where u.curso = 233
 and value(o).ano_letivo = '2004/2005'
@@ -10,7 +10,7 @@ group by value(ta).tipo
 /*b*/
 
 create or replace view requiredHours as
-select value(u).codigo as codigo, sum(value(ta).n_aulas* value(ta).turnos) as horas
+select value(u).codigo as codigo, sum(value(ta).horas_turno* value(ta).turnos) as horas
 from ucs u, table(value(u).ocorrencias) o, table(value(o).tiposAula) ta
 where value(o).ano_letivo = '2003/2004'
 group by value(u).codigo;
